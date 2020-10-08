922e8261053930512b7e0de79eb482b32341c4f5
Joshua_Vaquerano <32326248+josh7helegend@users.noreply.github.com>
2020-10-08 07:53:15 -0600

    Add files via upload

--- Lab2_2014011348.py
+++ Lab2_2014011348.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[24]:
+# In[26]:
 
 
 import numpy as np
 import time
 
+#En esta seccion se establece la variable de tiempo y la funcion que generara los numeros aleatorios y su distribucion normal
 inicio = time.time()
-DNNA=np.random.normal(loc=500,scale=30,size=10000000)
+DNNA=np.random.normal(loc=500,scale=30,size=10000000) 
+
+#En esta seccion inicia la iteracion de datos mediante la estructura de control for
 SVMQ=0
-
-
 for valor in DNNA:
     if valor <=500000:
         SVMQ+=valor
         
-
 print('La sumatoria de los numeros menores a 500,000 es:',SVMQ)
 print('Duracion: {} segundos'.format(time.time() - inicio))
 
+
+# DNNA=Distrbucion normal de numeros aleatorios
+# SVQM=Sumatoria de valores menores a quinientos mil
 
 # In[ ]:
 
